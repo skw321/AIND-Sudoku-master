5cd339f110b7d57deb9e8cb4d9cf557e4fdc21fc
Shiau Kang Wang <skwuslife@gmail.com>
2017-07-31 17:24:38 -0500

    Add comment to diag sudoku

--- solution.py
+++ solution.py
@@ -54,14 +54,17 @@ def cross(A, B):
 	
 rows = 'ABCDEFGHI'
 cols = '123456789'
-
 boxes = cross(rows, cols)
-
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
+
+#add a new constraint for diagonal diagonal sudoku
 diag_units = [[rows[i] + cols[i] for i in range(9)], [rows[::-1][i] + cols[i] for i in range(9)]]
+
+#add it to unitlist 
 unitlist = row_units + column_units + square_units + diag_units
+
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 	
